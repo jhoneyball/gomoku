b1c774abd849d00f9bda62bc457222da1c622be0
James <james@honeyball.com>
2016-10-29 14:29:55 +0100

    adjusted rect colour

--- Gomoku/GridView.swift
+++ Gomoku/GridView.swift
@@ -9,7 +9,7 @@ class GridView: UIView {
     init (frame: CGRect, board: Board){
         self.board = board
         super.init(frame: frame)
-        self.backgroundColor = UIColor(colorLiteralRed: 255/255.0, green: 226/255.0, blue: 154/255.0, alpha: 1)
+        self.backgroundColor = UIColor(colorLiteralRed: 255/255.0, green: 226/255.0, blue: 226/255.0, alpha: 1)
     }
     
     required init?(coder aDecoder: NSCoder) {
