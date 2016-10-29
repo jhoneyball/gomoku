d6f6c2f6d17d31fc9179a675c52210609fd06155
James <james@honeyball.com>
2016-10-29 21:27:18 +0100

    draw lines on board

--- Gomoku/GridView.swift
+++ Gomoku/GridView.swift
@@ -16,9 +16,25 @@ class GridView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
   
-    
-//    override func drawrect(rect: CGRect) {
-//
-//    }
 
+    
+    override func draw(_ rect: CGRect) {
+        
+        let outerBoxSize = min (rect.size.width, rect.size.height)
+        let cellCount = board.getWidth() + 1
+        let cellSize = outerBoxSize / CGFloat(board.getWidth()) + 1.0
+        
+        let context = UIGraphicsGetCurrentContext()
+        let path = UIBezierPath()
+        for i in 1..<cellCount {
+            let xPosition = CGFloat(i) * cellSize
+            path.move(to: CGPoint(x: xPosition, y: cellSize))
+            path.addLine(to: CGPoint(x: xPosition, y: outerBoxSize - cellSize))
+        }
+        
+        path.lineWidth = 1
+        path.stroke()
+        context!.addPath (path.cgPath)
+        UIGraphicsEndImageContext()
+    }
 }
