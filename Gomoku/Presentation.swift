a0893dc8395c2e73b44ce67294fed11b52239012
James <james@honeyball.com>
2016-10-30 14:43:35 +0000

    Sunday morning - about to refactor grid drawing

---
+++ Gomoku/Presentation.swift
@@ -0,0 +1,51 @@
+//
+//  Presentation.swift
+//  Gomoku
+//
+//  Created by James Honeyball on 30/10/2016.
+//  Copyright © 2016 Honeyball. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct TwoPoints {
+    var start: CGPoint
+    var finish: CGPoint
+}
+
+
+func calculateGoBoardLines (width: CGFloat, height: CGFloat, columns: Int, rows: Int) -> [TwoPoints]{
+    var lines = [TwoPoints]()
+
+    let multiplierForBottomOfBoard = CGFloat(rows) / (CGFloat(rows) + 1.0)
+    let multiplierForTopOfBoard = 1.0 / (CGFloat(rows) + 1.0)
+
+    for column in 1...columns {
+
+        let x = CGFloat(column) * (width / (CGFloat(columns) + 1.0))
+        let coOrdinateAX = x
+        let coOrdinateAY = multiplierForTopOfBoard * height
+        let coOrdinateBX = x
+        let coOrdinateBY = multiplierForBottomOfBoard * height
+
+        lines.append(TwoPoints(start:CGPoint(x:coOrdinateAX, y:coOrdinateAY), finish:CGPoint(x:coOrdinateBX, y:coOrdinateBY)))
+    }
+
+    let multiplierForLeftOfBoard = 1.0 / (CGFloat(columns) + 1.0)
+    let multiplierForRightOfBoard = CGFloat(columns) / (CGFloat(columns) + 1.0)
+ 
+    for row in 1...rows {
+        
+        let y = CGFloat(row) * (height / (CGFloat(rows) + 1.0))
+        let coOrdinateAX = multiplierForLeftOfBoard * width
+        let coOrdinateAY = y
+        let coOrdinateBX = multiplierForRightOfBoard * width
+        let coOrdinateBY = y
+        
+        
+        lines.append(TwoPoints(start:CGPoint(x:coOrdinateAX, y:coOrdinateAY), finish:CGPoint(x:coOrdinateBX, y:coOrdinateBY)))
+    }
+    
+    return lines
+}
---
+++ GomokuTests/PresentationTest.swift
@@ -0,0 +1,52 @@
+
+import XCTest
+import UIKit
+
+@testable import Gomoku
+
+class PresentationTest: XCTestCase {
+    
+    func testCoOrdsCorrectFor3x3() {
+
+        var linesToDraw = calculateGoBoardLines (width: 100.0, height: 100.0, columns: 3, rows: 3)
+       
+        // Set up points for lines
+        let cGPoint00 = CGPoint(x: CGFloat(25.0), y: CGFloat(25.0))
+        let cGPoint01 = CGPoint(x: CGFloat(25.0), y: CGFloat(50.0))
+        let cGPoint02 = CGPoint(x: CGFloat(25.0), y: CGFloat(75.0))
+        let cGPoint10 = CGPoint(x: CGFloat(50.0), y: CGFloat(25.0))
+        let cGPoint12 = CGPoint(x: CGFloat(50.0), y: CGFloat(75.0))
+        let cGPoint20 = CGPoint(x: CGFloat(75.0), y: CGFloat(25.0))
+        let cGPoint21 = CGPoint(x: CGFloat(75.0), y: CGFloat(50.0))
+        let cGPoint22 = CGPoint(x: CGFloat(75.0), y: CGFloat(75.0))
+        
+        // Set up lines
+        let line00 = TwoPoints (start: cGPoint00, finish: cGPoint02)
+        let line01 = TwoPoints (start: cGPoint10, finish: cGPoint12)
+        let line02 = TwoPoints (start: cGPoint20, finish: cGPoint22)
+        let line03 = TwoPoints (start: cGPoint00, finish: cGPoint20)
+        let line04 = TwoPoints (start: cGPoint01, finish: cGPoint21)
+        let line05 = TwoPoints (start: cGPoint02, finish: cGPoint22)
+        
+
+        XCTAssertEqual (line00.start, linesToDraw[0].start)
+        XCTAssertEqual (line00.finish, linesToDraw[0].finish)
+
+        XCTAssertEqual (line01.start, linesToDraw[1].start)
+        XCTAssertEqual (line01.finish, linesToDraw[1].finish)
+
+        XCTAssertEqual (line02.start, linesToDraw[2].start)
+        XCTAssertEqual (line02.finish, linesToDraw[2].finish)
+
+        XCTAssertEqual (line03.start, linesToDraw[3].start)
+        XCTAssertEqual (line03.finish, linesToDraw[3].finish)
+
+        XCTAssertEqual (line04.start, linesToDraw[4].start)
+        XCTAssertEqual (line04.finish, linesToDraw[4].finish)
+
+        XCTAssertEqual (line05.start, linesToDraw[5].start)
+        XCTAssertEqual (line05.finish, linesToDraw[5].finish)
+
+    }
+    
+}
