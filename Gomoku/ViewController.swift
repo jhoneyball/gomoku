bf5306ad3d5e497292570096caa01ce85967e9d0
James <james@honeyball.com>
2016-11-19 21:55:55 +0000

    check for changes

--- Gomoku/ViewController.swift
+++ Gomoku/ViewController.swift
@@ -13,14 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blue
-        let board = Board(columns: 19, rows: 19)
+        let board = Board(columns: 3, rows: 3)
         try! board.place(intersection: Intersection(1,1), player: Player.Black)
         try! board.place(intersection: Intersection(0,0), player: Player.White)
-        try! board.place(intersection: Intersection(11,11), player: Player.White)
-        try! board.place(intersection: Intersection(5,10), player: Player.Black)
-        try! board.place(intersection: Intersection(11,5), player: Player.White)
-        try! board.place(intersection: Intersection(12,5), player: Player.Black)
-        try! board.place(intersection: Intersection(18,18), player: Player.White)
+//        try! board.place(intersection: Intersection(11,11), player: Player.White)
+//        try! board.place(intersection: Intersection(5,10), player: Player.Black)
+//        try! board.place(intersection: Intersection(11,5), player: Player.White)
+//        try! board.place(intersection: Intersection(12,5), player: Player.Black)
+//        try! board.place(intersection: Intersection(18,18), player: Player.White)
 
         let gridView = GridView(frame: CGRect(x: 0, y: 75, width: self.view.frame.size.width, height: self.view.frame.size.width), board: board)
         self.view.addSubview(gridView)
