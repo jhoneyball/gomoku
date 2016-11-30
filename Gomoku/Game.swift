c3c9b2bb4525e61dc2a5429e5074be16de97c8e5
James <james@honeyball.com>
2016-11-30 01:10:06 +0000

    Updated win rules

--- Gomoku/Game.swift
+++ Gomoku/Game.swift
@@ -1,15 +1,20 @@
 class Game {
     let board: BoardProtocol
     var player = Player.White
+    var rules: GomokuRules
     
     init (board: BoardProtocol, rules: GomokuRules) {
         self.board = board
+        self.rules = rules
     }
     
     func takeTurn(intersection: Intersection) {
         if board.get(intersection: intersection) == Player.Empty {
             board.place(intersection: intersection, player: whosTurn())
-            player = other(player: player)          
+            if (rules.checkIsWin(board: board as! Board, intersection: intersection, player: player)) {
+                print ("Game Over")
+            }
+            player = other(player: player)
         }
     }
     
--- Gomoku/GomokuRules.swift
+++ Gomoku/GomokuRules.swift
@@ -18,4 +18,41 @@ class GomokuRules {
         }
         return fiveOrMoreInARow
     }
+    
+    func checkIsWin(board: Board, intersection: Intersection, player: Player) -> Bool {
+        var horizontal = 1
+        var cursor = intersection
+        
+        while board.right(of: cursor) != nil && board.get(intersection: board.right(of: cursor)!) == player {
+            horizontal += 1
+            cursor = board.right(of: cursor)!
+        }
+        cursor = intersection
+        while board.left(of: cursor) != nil && board.get(intersection: board.left(of: cursor)!) == player {
+            horizontal += 1
+            cursor = board.left(of: cursor)!
+        }
+        if horizontal >= 5 {
+            print ("Win for \(player)")
+            return true
+        }
+
+        var vertical = 1
+        cursor = intersection
+        while board.above(of: cursor) != nil && board.get(intersection: board.above(of: cursor)!) == player {
+            vertical += 1
+            cursor = board.above(of: cursor)!
+        }
+        cursor = intersection
+        while board.below(of: cursor) != nil && board.get(intersection: board.below(of: cursor)!) == player {
+            vertical += 1
+            cursor = board.below(of: cursor)!
+        }
+        if vertical >= 5 {
+            print ("Win for \(player)")
+            return true
+        }
+        print ("\(player) : \(horizontal) horizontal. \(vertical) vertical")
+    return false
+    }
 }
